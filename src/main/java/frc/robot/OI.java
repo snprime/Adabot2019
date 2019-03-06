b3cb4159bc94684fa55fa289dbbd8bacbfe8fce8
Steve <stnixon@gmail.com>
2019-03-05 23:51:20 -0800

    changed cargo shooter behavior

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -25,10 +25,11 @@ public class OI {
   public Joystick stick = new Joystick(RobotMap.stickPort);
   public Joystick throttle = new Joystick(RobotMap.throttle);
 
-  public JoystickButton intakeRunButton = new JoystickButton(stick, RobotMap.triggerHalf);
+  public JoystickButton fireCargoButton = new JoystickButton(stick, RobotMap.triggerHalf);
+  public JoystickButton intakeOffButton = new JoystickButton(stick, RobotMap.triggerFull);
   public JoystickButton intakeDeployButton = new JoystickButton(throttle, RobotMap.topRidgeForward);
   public JoystickButton intakeRetractButton = new JoystickButton(throttle, RobotMap.topRidgeBack);
-  public JoystickButton fireCargoButton = new JoystickButton(stick, RobotMap.redButton);
+  //public JoystickButton fireCargoButton = new JoystickButton(stick, RobotMap.redButton);
   public JoystickButton lowerHatchButton = new JoystickButton(stick, RobotMap.thumbUp);
   public JoystickButton raiseHatchButton = new JoystickButton(stick, RobotMap.thumbDown);
   public JoystickButton hatchSuctionButton = new JoystickButton(throttle, RobotMap.thumbForward);
@@ -37,16 +38,18 @@ public class OI {
 
   public OI(){
     //Intake Control
-    intakeRunButton.whenPressed(new intakeRun(-0.75));
-    intakeRunButton.whenReleased(new intakeRun(0.0));
+    fireCargoButton.whenPressed(new intakeRun(-0.75));
+    fireCargoButton.whenPressed(new fireCargo(-0.75));
+    intakeOffButton.whenPressed(new intakeRun(0.0));
+    intakeOffButton.whenReleased(new intakeRun(0.0));
 
     //Intake Deploy and Retract
     intakeDeployButton.whenPressed(new intakeDeploy());
     intakeRetractButton.whenPressed(new intakeRetract());
     
     //Cargo Ball Launcher Control
-    fireCargoButton.whenPressed(new fireCargo(-0.75));
-    fireCargoButton.whenReleased(new fireCargo(0.0));
+    //fireCargoButton.whenPressed(new fireCargo(-0.75));
+    //fireCargoButton.whenReleased(new fireCargo(0.0));
 
     //Hatch Deploy and Retract
     lowerHatchButton.whenPressed(new hatchDown());
